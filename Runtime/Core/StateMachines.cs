efb8b8d6a4fc19536e18414ea7cbb661b26b0ee6
ACELANDWORKSHOP\parsu <parsue@gmail.com>
2024-09-21 13:43:00 +0800

    remove Logging in StateMachines.Register

--- Runtime/Core/StateMachines.cs
+++ Runtime/Core/StateMachines.cs
@@ -2,19 +2,18 @@
 using System.Collections.Generic;
 using AceLand.Library.Extensions;
 using AceLand.States.ProjectSetting;
-using UnityEngine;
 
 namespace AceLand.States.Core
 {
     internal static class StateMachines
     {
         private static StatesSettings Settings => StatesHelper.Settings;
-        private static readonly Dictionary<string, IAnyStateMachine> _machines = new();
+        private static readonly Dictionary<string, IAnyStateMachine> Machines = new();
 
         internal static bool TryGetMachine(string id, out IStateMachine machine)
         {
             machine = null;
-            if (!_machines.TryGetValue(id, out var anyMachine)) return false;
+            if (!Machines.TryGetValue(id, out var anyMachine)) return false;
             if (anyMachine is not IStateMachine stateMachine) return false;
 
             machine = stateMachine;
@@ -24,37 +23,17 @@ namespace AceLand.States.Core
         internal static void Register(this IAnyStateMachine machine)
         {
             var id = machine.Id;
-            if (!_machines.TryAdd(id, machine))
+            if (!Machines.TryAdd(id, machine))
                 throw new Exception($"Add Machine Error: id [{id}] already exists");
-
-            if (Settings.enableLogging)
-            {
-#if UNITY_EDITOR
-                if (!Settings.loggingOnEditor) return;
-#else
-                if (!Settings.loggingOnBuild) return;
-#endif
-                Debug.Log($"State Machine [{id}] Registered");
-            }
         }
 
         internal static void UnRegister(this IAnyStateMachine machine)
         {
             var id = machine.Id;
             if (id.IsNullOrEmptyOrWhiteSpace()) return;
-            if (!_machines.ContainsKey(id)) return;
+            if (!Machines.ContainsKey(id)) return;
             
-            _machines.Remove(id);
-
-            if (Settings.enableLogging)
-            {
-#if UNITY_EDITOR
-                if (!Settings.loggingOnEditor) return;
-#else
-                if (!Settings.loggingOnBuild) return;
-#endif
-                Debug.Log($"State Machine [{id}] UnRegistered");
-            }
+            Machines.Remove(id);
         }
     }
 }
