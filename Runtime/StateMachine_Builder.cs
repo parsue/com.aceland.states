832edb5101eb69f054ad6cfd67812d65065f87b9
ACELANDWORKSHOP\parsu <parsue@gmail.com>
2024-10-30 20:38:11 +0800

    partial State Machine

---
+++ Runtime/StateMachine_Builder.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using AceLand.Library.Optional;
+using AceLand.PlayerLoopHack;
+using AceLand.States.Core;
+
+namespace AceLand.States
+{
+    public partial class StateMachine
+    {
+        public static IStatesBuilder Builder() => new StateMachineBuilder();
+
+        public interface IStatesBuilder
+        {
+            IEntryTransitionBuilder WithStates(IState[] states);
+        }
+        
+        public interface IEntryTransitionBuilder
+        {
+            IStateMachineBuilder WithEntryTransition(IState toState);
+        }
+
+        public interface IStateMachineBuilder
+        {
+            IStateMachineBuilder WithExitTransition(IState fromState, Func<bool> argument);
+            IStateMachineBuilder WithAnyExitTransition(Func<bool> argument);
+            IStateMachineBuilder WithAnyTransition(IState toState, Func<bool> argument, bool preventToSelf = true);
+            IStateMachineBuilder WithTransition(IState fromState, IState toState, Func<bool> argument, bool preventToSelf = true);
+            IStateMachineBuilder WithId(string id);
+            IStateMachineBuilder WithId<T>(T id) where T : Enum;
+            IStateMachine Build();
+            IStateMachine BuildAsSystem(PlayerLoopType playerLoopType, int index = 0);
+        }
+
+        private class StateMachineBuilder : IStatesBuilder, IEntryTransitionBuilder, IStateMachineBuilder
+        {
+            private IState[] _states;
+            private readonly List<StateTransition> _anyTransitions = new();
+            private readonly List<StateTransition> _transitions = new();
+            private IAnyState _firstState;
+            private string _id;
+
+            public IStateMachine Build() => 
+                new StateMachine(_id.ToOption(), _states, _firstState, _anyTransitions, _transitions);
+            
+            public IStateMachine BuildAsSystem(PlayerLoopType playerLoopType, int index = 0) =>
+                new StateMachineSystem(_id.ToOption(), _states, _firstState, _anyTransitions, _transitions, playerLoopType, index);
+
+            public IEntryTransitionBuilder WithStates(IState[] states)
+            {
+                _states = states;
+                return this;
+            }
+            
+            public IStateMachineBuilder WithEntryTransition(IState toState)
+            {
+                _firstState = toState;
+                return this;
+            }
+
+            public IStateMachineBuilder WithExitTransition(IState fromState, Func<bool> argument)
+            {
+                var toState = StatesHelper.ExitState;
+                _anyTransitions.Add(StatesHelper.CreateTransition(fromState, toState, argument, true));
+                return this;
+            }
+
+            public IStateMachineBuilder WithAnyExitTransition(Func<bool> argument)
+            {
+                var fromState = StatesHelper.AnyState;
+                var toState = StatesHelper.ExitState;
+                _anyTransitions.Add(StatesHelper.CreateTransition(fromState, toState, argument, true));
+                return this;
+            }
+            
+            public IStateMachineBuilder WithAnyTransition(IState toState, Func<bool> argument, bool preventToSelf = true)
+            {
+                var fromState = StatesHelper.AnyState;
+                _anyTransitions.Add(StatesHelper.CreateTransition(fromState, toState, argument, preventToSelf));
+                return this;
+            }
+
+            public IStateMachineBuilder WithAnyTransition(IIdleState toState, Func<bool> argument, bool preventToSelf = true)
+            {
+                var fromState = StatesHelper.AnyState;
+                _anyTransitions.Add(StatesHelper.CreateTransition(fromState, toState, argument, preventToSelf));
+                return this;
+            }
+
+            public IStateMachineBuilder WithTransition(IState fromState, IState toState, Func<bool> argument, bool preventToSelf = true)
+            {
+                _transitions.Add(StatesHelper.CreateTransition(fromState, toState, argument, preventToSelf));
+                return this;
+            }
+
+            public IStateMachineBuilder WithTransition(IIdleState fromState, IIdleState toState, Func<bool> argument, bool preventToSelf = true)
+            {
+                _transitions.Add(StatesHelper.CreateTransition(fromState, toState, argument, preventToSelf));
+                return this;
+            }
+
+            public IStateMachineBuilder WithTransition(IState fromState, IIdleState toState, Func<bool> argument, bool preventToSelf = true)
+            {
+                _transitions.Add(StatesHelper.CreateTransition(fromState, toState, argument, preventToSelf));
+                return this;
+            }
+
+            public IStateMachineBuilder WithTransition(IIdleState fromState, IState toState, Func<bool> argument, bool preventToSelf = true)
+            {
+                _transitions.Add(StatesHelper.CreateTransition(fromState, toState, argument, preventToSelf));
+                return this;
+            }
+
+            public IStateMachineBuilder WithId(string id)
+            {
+                _id = id;
+                return this;
+            }
+
+            public IStateMachineBuilder WithId<T>(T id) where T : Enum
+            {
+                _id = id.ToString();
+                return this;
+            }
+        }
+    }
+}
---
+++ Runtime/StateMachine_Getter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AceLand.Library.Optional;
+using AceLand.PlayerLoopHack;
+using AceLand.States.Core;
+using AceLand.TaskUtils;
+using AceLand.TaskUtils.PromiseAwaiter;
+using UnityEngine;
+
+namespace AceLand.States
+{
+    public partial class StateMachine
+    {
+        public static Promise<IStateMachine> Get(string id) => GetStateMachine(id);
+        public static Promise<IStateMachine> Get<T>(T id) where T : Enum => GetStateMachine(id.ToString());
+
+        private static async Task<IStateMachine> GetStateMachine(string id)
+        {
+            var aliveToken = TaskHelper.ApplicationAliveToken;
+            var targetTime = Time.realtimeSinceStartup + Settings.getterTimeout;
+
+            while (!aliveToken.IsCancellationRequested && Time.realtimeSinceStartup < targetTime)
+            {
+                await Task.Yield();
+                
+                var arg = StateMachines.TryGetMachine(id, out var stateMachine);
+                
+                if (arg) return stateMachine;
+            }
+
+            throw new Exception($"State Machine [{id}] is not found");
+        }
+    }
+}
